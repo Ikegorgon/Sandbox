070a64ee6c12afc59ea37f1cc215818e02dfe982
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-04-16 11:57:16 -0600

    Added a Frame

---
+++ src/sandbox/view/SandboxFrame.java
@@ -0,0 +1,34 @@
+package sandbox.view;
+
+import sandbox.controller.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class SandboxFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private SandboxController app;
+	private SandboxPanel appPanel;
+	public SandboxFrame() {
+		
+	}
+	public SandboxFrame(SandboxController app) {
+		super();
+		this.app = app;
+		appPanel = new SandboxPanel(app);
+		setupFrame();
+	}
+	private void setupFrame() {
+		this.setContentPane(appPanel);
+		this.setTitle("Sandbox");
+		this.setSize(1000, 1000);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setName("Sandbox");
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass()
+				.getResource("/sandbox/view/images/sandboxIcon.png")));
+	}
+	public SandboxController getController() {
+		return app;
+	}
+}
